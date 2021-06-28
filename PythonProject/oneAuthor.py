5da61faf0f0f886374eaa1433c6c8bf5dab8d2d9
matthewloden <85366421+matthewloden@users.noreply.github.com>
2021-06-28 17:46:11 -0500

    Updated the program to run multiple entries at a time
    Still need to work on not hard coding the length of the file and getting the output to print to the csv correctly

--- PythonProject/oneAuthor.py
+++ PythonProject/oneAuthor.py
@@ -8,34 +8,70 @@ import csv
 ##############  Opening CSV File called test_Names
 ###############################################################################################################################################################################
 ###############################################################################################################################################################################
+myvar = [None]*4
 with open('test_Names.csv', mode = 'r') as test_Names:
-    fieldnames = ['first_Name', 'last_Name', 'University','Blank', 'Citations']
     csv_reader = csv.reader(test_Names)
-    for i, line in enumerate(csv_reader): 
-            if(i > 0):
-                myRequest = line['first_Name'] + " " + line['last_Name'] + ", " + line['University']
-                search_query = scholarly.search_author(myRequest)
-                
-                authors = scholarly.fill(next(search_query))
+    for i,line in enumerate(csv_reader):
+        myvar[i] = line
 
-                #authors['publications'][0]['bib']['title']
+#print(myvar[1][0])
+myRequest = [None]*4
+for j in range(4):
+    myRequest[j] = myvar[j][0] + ' ' + myvar[j][1] + ', ' + myvar[j][2]
+#print(myRequest)
+myResult = [None]*150
+fieldnames = myvar[0]
+myRequest.pop(0)
+myOutput = [None]*4
+myvar.pop(0)
+for item in myRequest:
+    print("Working on: ",item)
+    searchQuery = scholarly.search_author(item)
+    authors = scholarly.fill(next(searchQuery))
 
-                for i, publication in enumerate(authors['publications']):
-                    print(publication['bib']['title'])
-                    #line[i + 5] = publication['bib']['title']
+    for i, publication in enumerate(authors['publications']):
+        #print(publication['bib']['title'])
+        if i < 149:
+            myResult[i] = publication['bib']['title'].encode('utf-8').decode('ascii','ignore')
+    for i in range(3):
+        myOutput[i] = myvar[i]+myResult
+        print(myOutput[i])
+#print(myResult)
+
+try:
+    with open('newNames.csv','w',newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(fieldnames)
+        for i in range(3):
+            csv_writer.writerow(myOutput[i])
+except:
+    print("An error has occured within the print to csv function")
+
+# for i, line in enumerate(csv_reader): 
+#         if(i > 0):
+#             myRequest = line['first_Name'] + " " + line['last_Name'] + ", " + line['University']
+#             search_query = scholarly.search_author(myRequest)
+            
+#             authors = scholarly.fill(next(search_query))
+
+#             #authors['publications'][0]['bib']['title']
+
+#             for i, publication in enumerate(authors['publications']):
+#                 print(publication['bib']['title'])
+#                 #line[i + 5] = publication['bib']['title']
                     
             
         
-###############################################################################################################################################################################
-###############################################################################################################################################################################
-##############  Writing to CSV File called new_Names
-###############################################################################################################################################################################
-###############################################################################################################################################################################
-try:
-    with open('new_Names.csv', mode = 'w') as new_Names:
-        csv_writer = csv.writer(new_Names)
-except:
-    print("An error has occured within the print to csv function")
+# ###############################################################################################################################################################################
+# ###############################################################################################################################################################################
+# ##############  Writing to CSV File called new_Names
+# ###############################################################################################################################################################################
+# ###############################################################################################################################################################################
+# try:
+#     with open('new_Names.csv', mode = 'w') as new_Names:
+#         csv_writer = csv.writer(new_Names)
+# except:
+#     print("An error has occured within the print to csv function")
         
 
 
